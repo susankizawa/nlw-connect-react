5cd0c198f93cdc97ca0fc46597e26032111fb23b
Susan K. Izawa <susan.k.izawa@gmail.com>
2025-03-03 05:07:08 -0300

    Made it so the copy link button changes from the copy icon to the check icon for 1s when clicked

--- src/app/invite/[subscriberId]/invite-link-input.tsx
+++ src/app/invite/[subscriberId]/invite-link-input.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+// Importing external dependencies
+import { useState } from 'react'
+
 // Importing images
-import { Copy, Link } from 'lucide-react'
+import { Check, Copy, Link } from 'lucide-react'
 
 // Importing local components
 import { IconButton } from '@/components/icon-button'
@@ -16,7 +19,19 @@ interface InviteLinkInputProps {
 }
 
 export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  const [isCopied, setIsCopied] = useState(false)
+
   function copyInviteLink() {
+    // Sets isCopied to true for 1 second which results in the copy icon being shown by default
+    // until the copy link button is clicked, then it changes to a check icon for 1s before
+    // reverting to the copy icon
+
+    setIsCopied(true)
+
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 1000) // 1000ms = 1s
+
     navigator.clipboard.writeText(inviteLink)
   }
 
@@ -26,8 +41,9 @@ export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
         <Link className="size-5" />
       </TextInputIcon>
       <TextInputField defaultValue={inviteLink} readOnly />
-      <IconButton className="-mr-2">
-        <Copy className="size-5" onClick={copyInviteLink} />
+      <IconButton className="-mr-2 relative size-8" onClick={copyInviteLink}>
+        {/* Shows Check icon if isCopied is true and a Copy icon if not */}
+        {isCopied ? <Check className="size-5" /> : <Copy className="size-5" />}
       </IconButton>
     </TextInputRoot>
   )
