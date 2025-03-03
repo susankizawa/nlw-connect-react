4e16dc64341692f1ff046abf23f2eb4895c8bb20
Susan K. Izawa <susan.k.izawa@gmail.com>
2025-03-03 03:50:17 -0300

    Updated invite link and orval to use an environment variable for the backend url instead of being hardcoded
    How to update API methods generated by Orval in Windows:
    $env:BACKEND_URL=YOUR_URL; npx orval

--- orval.config.ts
+++ orval.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'orval'
 
 export default defineConfig({
   api: {
-    input: 'https://nlw-connect-node-js.onrender.com/docs/json',
+    input: `${process.env.BACKEND_URL}/docs/json`,
     output: {
       target: './src/http/api.ts',
       client: 'fetch',
       httpClient: 'fetch',
       clean: true,
-      baseUrl: 'https://nlw-connect-node-js.onrender.com',
+      baseUrl: process.env.BACKEND_URL,
       override: {
         fetch: {
           includeHttpResponseReturnType: false,
--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -18,7 +18,7 @@ interface InviteProps {
 export default async function Invite(props: InviteProps) {
   const { subscriberId } = await props.params
 
-  const inviteLink = `https://nlw-connect-node-js.onrender.com/invites/${subscriberId}`
+  const inviteLink = `${process.env.BACKEND_URL}/invites/${subscriberId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
